eaf5bec9a591778108951252796959bf9a1f7c72
Kelvin Homann <kelvin.homann@gmail.com>
2019-02-10 13:06:54 +0100

    Added new Models for Castle and Characters;

---
+++ Assets/Scripts/Castle/Castle.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Castle : MonoBehaviour {
+    public int health = 100;
+
+    public GameObject intact;
+    public GameObject damaged;
+    public GameObject ruin;
+    public GameObject pieces;
+
+    public ParticleSystem particles;
+
+    private bool destroyed_once = false;
+    private bool destroyed_twice = false;
+
+    void Start () {
+        particles.Stop();
+        // Disabling the Damaged Models
+        intact.SetActive(true);
+        damaged.SetActive(false);
+        ruin.SetActive(false);
+        pieces.SetActive(false);
+    }
+	
+	void Update () {
+        if (health < 0)
+            health = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            health = health - 20;
+        } 
+        
+        if (health <= 40) // First Destruction
+        {
+            if (!destroyed_once)
+            {
+                particles.Play();
+                damaged.SetActive(true);
+                ruin.SetActive(false);
+                intact.SetActive(false);
+                destroyed_once = true;
+            }            
+        }
+
+        if (health <= 0) // Second Destruction
+        {
+            if (!destroyed_twice)
+            {
+                particles.Play();
+                ruin.SetActive(true);
+                damaged.SetActive(false);
+                pieces.SetActive(true);
+                destroyed_twice = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Castle/PiecesFadeOut.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiecesFadeOut : MonoBehaviour {
+
+    MeshRenderer meshRenderer;
+
+    private bool fadingout = false;
+
+    void Start () {
+        meshRenderer = this.GetComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial.SetFloat("_Amount", 0f);
+    }
+
+    IEnumerator FadeOut()
+    {
+//        yield return new WaitForSecondsRealtime(Random.Range(2f, 5f));
+        yield return new WaitForSecondsRealtime(4);
+
+        for (float i = 0; i < Random.Range(0.7f, 0.9f); i += 0.005f)
+        {
+            meshRenderer.sharedMaterial.SetFloat("_Amount", i);
+            yield return new WaitForSeconds(0.05f);
+        }
+        Destroy(gameObject);
+    }
+
+    public void startFading()
+    {
+        StartCoroutine("FadeOut");
+    }
+
+    public void Update()
+    {
+        if (!fadingout && this.isActiveAndEnabled)
+        {
+            startFading();
+            fadingout = true;
+        }
+    }
+}
