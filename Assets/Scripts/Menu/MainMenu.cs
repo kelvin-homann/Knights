f267323619e16f3ee60f0fef7809bdf3ec4f7d25
Kelvin Homann <kelvin.homann@gmail.com>
2019-02-11 16:28:06 +0100

    replaced purple flag with yellow flag, added textmeshpro asset, added ui elements to main menu;

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public float transitionSpeed;
+    public Transform[] targets;
+    Transform currentTarget;
+    public Camera camera;
+
+    public void Start()
+    {
+        currentTarget = targets[0];
+    }
+
+    public void SelectPlayers()
+    {
+        // Move Camera to Flags      
+        currentTarget = targets[1];
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    private void LateUpdate()
+    {
+        camera.transform.position = Vector3.Lerp(transform.position, currentTarget.position, Time.deltaTime * transitionSpeed);
+        Vector3 currentAngle = new Vector3(Mathf.LerpAngle(camera.transform.rotation.eulerAngles.x, 
+            currentTarget.transform.rotation.eulerAngles.x, Time.deltaTime * transitionSpeed),
+            Mathf.LerpAngle(camera.transform.rotation.eulerAngles.y, currentTarget.transform.rotation.eulerAngles.y,
+            Time.deltaTime * transitionSpeed), Mathf.LerpAngle(camera.transform.rotation.eulerAngles.z, currentTarget.transform.rotation.eulerAngles.z,
+            Time.deltaTime * transitionSpeed));
+
+        camera.transform.eulerAngles = currentAngle;
+
+    }
+}
